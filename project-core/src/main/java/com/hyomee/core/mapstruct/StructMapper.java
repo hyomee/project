3016557bc82e16f52d7fda095573c5158c0ea20b
hyosang <hyomee@naver.com>
2023-08-31 01:38:59 +0900

    StructMapper 인터페이스

---
+++ project-core/src/main/java/com/hyomee/core/mapstruct/StructMapper.java
@@ -0,0 +1,15 @@
+package com.hyomee.core.mapstruct;
+
+import java.util.List;
+
+public interface StructMapper<D,E> {
+
+    // D : DTO , E : Entity
+
+    D toDTO(E entity);
+    List<D> toDTOs(List<E> entities);
+
+    E toEntity(D dto);
+    List<E> toEntities (List<D> DTOs);
+
+}
